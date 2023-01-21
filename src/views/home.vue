da69bf09274c5b4712440fbdd159f707d3a45f80
QuadnucYard <2380433991@qq.com>
2023-01-21 10:49:45 +0800

    Complete home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-    <SearchBox/>
+  <div class="home">
+    <SearchBox />
+    <el-carousel height="300px">
+      <el-carousel-item v-for="item in carouselImages" :key="item">
+        <el-image :src="getImageUrl(item)" fit="fill" />
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script setup lang="ts">
-import SearchBoxVue from '@/components/SearchBox.vue';
+import _ from "lodash";
+
+const carouselImages = _.range(1, 7).map(t => `${t}.jpg`);
+
+function getImageUrl(name: string) {
+  return new URL(`../assets/img/carousel/${name}`, import.meta.url).href;
+}
 </script>
 
 <style lang="scss">
-  
+.home {
+  .el-carousel {
+    width: 800px;
+    margin: auto;
+    margin-top: 20px;
+  }
+}
 </style>
